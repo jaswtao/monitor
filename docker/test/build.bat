6558a23ab14b4a3e189d4982b9562efadf9c8089
terrytao <taoli51@gmail.com>
2020-07-04 17:55:54 +0800

    update docker config

--- docker/test/build.bat
+++ docker/test/build.bat
@@ -8,8 +8,6 @@ copy /Y target\*.jar docker\test\lib\
 
 cd docker/test
 
-call docker build -t 10.1.8.40:1080/csci/monitor:test-v1.0.1 .
+call docker build -t localhost:1080/dhome/monitor:test-v1.0.1 .
 
-call docker push 10.1.8.40:1080/csci/monitor:test-v1.0.1
-
-@rem docker service update --image 10.1.8.40:1080/csci/financial:test-v1.0.15 fin-backend
+@rem call docker push localhost:1080/dhome/monitor:test-v1.0.1
